fa000c0bac8b953dc41c96eeecd23abb4fbcb66c
Weiss lumumba <weissstephen98@gmail.com>
2024-12-13 17:18:29 +0300

    1st part of project structure

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,125 +1,68 @@
+// Flutter package imports
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
-void main() {
-  runApp(const MyApp());
+// Screens
+import 'package:app/Screens/Login-Signup/Profile_screen.dart';
+import 'package:app/Screens/Views/Dashboard_screen.dart';
+import 'package:app/Screens/Views/Homepage.dart';
+import 'package:app/Screens/Views/Screen1.dart';
+import 'package:app/Screens/Views/appointment.dart';
+import 'package:app/Screens/Views/chat_screen.dart';
+import 'package:app/Screens/Views/doctor_search.dart';
+import 'package:app/Screens/Widgets/tab-barPages/message_tab.dart';
+import 'package:app/Screens/Widgets/tab-barPages/tab1.dart';
+import 'package:app/Screens/Widgets/article.dart';
+import 'package:app/Screens/Views/article_page.dart';
+import 'package:app/Screens/Widgets/doctor_list.dart';
+import 'package:app/Screens/Views/message_Screen.dart';
+import 'package:app/Screens/Views/find_doctor.dart';
+import 'package:app/Screens/Views/doctor_details_screen.dart';
+
+// Authentication & Settings screens
+import 'package:app/Screens/Login-Signup/forgot_password.dart';
+import 'package:app/Screens/Login-Signup/login.dart';
+import 'package:app/Screens/Login-Signup/login_signup.dart';
+import 'package:app/Screens/Login-Signup/sign_up.dart';
+import 'package:app/Screens/Login-Signup/verification_code.dart';
+import 'package:app/Screens/Login-Signup/schedule_screen.dart';
+
+// Onboarding screens
+import 'package:app/Screens/On_Board/on_boarding.dart';
+
+
+vvoid main() {
+  runApp(const Medics());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Medics extends StatelessWidget {
+  const Medics({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ResponsiveSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,  // Hide debug banner in production
+          theme: ThemeData(
+            primarySwatch: Colors.blue,  // Primary color for the app
+            textTheme: TextTheme(
+              bodyLarge: TextStyle(fontSize: 16.sp),  // Make font size responsive
+            ),
+          ),
+          initialRoute: '/',  // Set the initial screen to be loaded
+          routes: {
+            '/': (context) => const Screen1(),  // Home screen
+            '/login': (context) => const Login(),  // Login screen
+            '/register': (context) => const Register(),  // Registration screen
+            '/dashboard': (context) => const DashboardScreen(),  // Dashboard screen
+            '/profile': (context) => const ProfileScreen(),  // Profile screen
+            '/article': (context) => const ArticlePage(),  // Article page
+            // Add other screens here
+          },
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
