f191dccc12b82a0427ba5695ec255cf21dfacd60
weissssssss <97245417+weissssssss@users.noreply.github.com>
2024-12-13 13:40:20 +0300

    Delete telemedicine_app directory
    issues with project structure

--- telemedicine_app/android/app/src/main/kotlin/com/example/telemedicine_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.telemedicine_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- telemedicine_app/lib/utils/constants/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String homeRoute = '/';
-  static const String loginRoute = '/login';
-  static const String appointmentsRoute = '/appointments';
-  static const String profileRoute = '/profile';
-}
